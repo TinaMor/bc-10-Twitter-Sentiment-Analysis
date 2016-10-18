66bfa23a1d36a5bd6548161e7542e557b3175758
Michael Kamau <tokamau@gmail.com>
2016-10-18 10:40:33 +0300

    Add tweets module with tweet processing functions

---
+++ twitter_sentiment_analysis/tweets.py
@@ -0,0 +1,14 @@
+import tweepy
+from twitter_sentiment_analysis import authentication
+
+api = authentication.authenticate()
+
+tweets_list = []
+
+
+def get_last_200_tweets():
+    """
+    Gets the latest 200 tweets if available
+    """
+    for status in tweepy.Cursor(api.user_timeline).items(200):
+        tweets_list.append(status)
