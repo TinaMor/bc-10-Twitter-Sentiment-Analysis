fad23e68f562e9bf9e1883ab7a2b3e6422a1c1e4
Michael Kamau <tokamau@gmail.com>
2016-10-18 11:13:41 +0300

    Add function to save all tweets to JSON file

--- twitter_sentiment_analysis/tweets.py
+++ twitter_sentiment_analysis/tweets.py
@@ -1,6 +1,8 @@
 import tweepy
+import json
 from twitter_sentiment_analysis import authentication
 
+# TODO: Wrap this in function -- init_api()
 api = authentication.authenticate()
 
 tweets_list = []
@@ -10,5 +12,14 @@ def get_last_200_tweets():
     """
     Gets the latest 200 tweets if available, add them to tweet_list
     """
-    for status in tweepy.Cursor(api.user_timeline).items(200):
-        tweets_list.append(status)
+    for status in tweepy.Cursor(api.user_timeline).items(2):
+        tweets_list.append(status._json)
+
+
+def save_tweets_json_file():
+    """
+    Save the collected tweets onto a JSON file
+    """
+    with open('allTweets.json', 'w') as f:
+        json.dump(tweets_list, f)
+
