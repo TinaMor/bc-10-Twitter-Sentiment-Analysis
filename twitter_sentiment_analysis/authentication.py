363b5a50bb8df85ed530e57a21b9f5df51c10e6e
Michael Kamau <tokamau@gmail.com>
2016-10-18 09:01:46 +0300

    Refactor authentication function to return API
    Returned object will be used to access twitter API directly.

--- twitter_sentiment_analysis/authentication.py
+++ twitter_sentiment_analysis/authentication.py
@@ -3,6 +3,7 @@ import tweepy
 consumer_key = 'hTM8QyARGUpNS96bBr31tkggf'
 consumer_secret = 'UHW3FRTwrNESX57NNLwE4I4s3DDcLpr9aYPphDDEadJcBa7LvS'
 
+
 def authenticate():
     # create OAuth Handler
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -10,6 +11,14 @@ def authenticate():
     # get authorisation url
     try:
         auth_url = auth.get_authorization_url(signin_with_twitter=True)
-        print(auth_url)
+        print("\nPlease go to ", auth_url, " to authorise this app to access your account.\n")
+        verifier = input("Enter PIN provided: ")
     except tweepy.TweepError:
-        print("Cannot get request token")
+        print("Cannot generate authorisation URL")
+
+    # request access token
+    try:
+        auth.get_access_token(verifier)
+        return tweepy.API(auth)
+    except tweepy.TweepError:
+        print("Failed to acquire access token")
