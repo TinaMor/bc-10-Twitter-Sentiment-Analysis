3c9eda8cbc2dd16ed54df024bf03e7ea5af7295d
Michael Kamau <tokamau@gmail.com>
2016-10-20 16:55:46 +0300

    Removed tests.

--- test/twitter_api_tests.py
+++ test/twitter_api_tests.py
@@ -1,11 +1,13 @@
 import unittest
 from twitter_sentiment_analysis import authentication
+
+
 class AuthenticationTests(unittest.TestCase):
     """
     Tests the authentication with Twitter
     """
-    def test_generates_auth_url(self):
-        """
-        Should be able to generate an authentication URL to enable user authorise the app
-        """
-        self.assertEqual(True, authentication.authenticate() != None)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
