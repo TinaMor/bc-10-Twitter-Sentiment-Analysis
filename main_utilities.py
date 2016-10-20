a68ddae5e92c0312e3363643aab92b67066b73c4
Michael Kamau <tokamau@gmail.com>
2016-10-20 12:38:46 +0300

    Displaying user info

--- main_utilities.py
+++ main_utilities.py
@@ -1,6 +1,6 @@
 from twitter_sentiment_analysis.utilities import bcolors
 from twitter_sentiment_analysis import tweets
-
+import subprocess
 
 def get_header():
     """
@@ -30,13 +30,27 @@ def get_info_warning():
     res += warn + bcolors.FAIL + "\n" + note + bcolors.ENDC + '\n' + bcolors.BOLD + top + "\n" + bcolors.ENDC
     return res
 
+def get_personal_info():
+    user = tweets.get_user_info()
+    print(bcolors.UNDERLINE, "Personal Details:", bcolors.ENDC, "\n")
+    nm = user['name']
+    name = ' '.join([n.capitalize() for n in nm.split(' ')])
+    print(bcolors.OKBLUE, "Name:", bcolors.ENDC, bcolors.OKGREEN, name, bcolors.ENDC, bcolors.OKBLUE, "\tTwitter Handle: ",
+          bcolors.ENDC, bcolors.OKGREEN, "@",user['screen_name'], bcolors.OKBLUE, "Number of Tweets: ", bcolors.ENDC, bcolors.OKGREEN, user['statuses_count'],bcolors.ENDC,
+          bcolors.OKBLUE, "Number of Followers: ", bcolors.ENDC, bcolors.OKGREEN, user['followers_count'], bcolors.ENDC)
+    print(bcolors.OKBLUE, "Latest Tweet:", bcolors.ENDC, bcolors.BOLD, user['status']['text'], bcolors.ENDC + "\n")
 
 def main_loop():
     print(get_header())
     print(get_info_warning())
 
     # Keep prompting for correct code
-    incorrect_code = False
-    while not incorrect_code:
-        tweets.init_api()
+    tweets.init_api()
+
+    # clear screen, print header
+    subprocess.call('clear', shell=True)
+    print(get_header())
+
+    # print personal info
+    get_personal_info()
 
